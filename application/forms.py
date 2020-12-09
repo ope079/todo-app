b3e42817315fc5508d91056464b1e4cc6bdc9a0d
ope079 <o_orekoya@yahoo.com>
2020-12-09 19:55:21 +0000

    added complete/incomplete

--- application/forms.py
+++ application/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, IntegerField, DecimalField, SelectField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, InputRequired
 
 class TaskForm(FlaskForm):
     description = StringField('Description of Task', validators=[DataRequired()])
+    complete = SelectField(choices=[(True, 'Yes'), (False, 'No')], 
+            validators=[InputRequired()],
+            validate_choice=True or False)
     submit = SubmitField('Add the Task')
+
